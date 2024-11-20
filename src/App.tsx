23e39834e7b95e7df225f8567f746400bed79f66
«Elena <LenaRybinskova@gmail.com>
2024-11-20 17:54:24 +0300

    theming with Body attr

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { PostsPage } from "../src/pages/PostsPage";
 import { Provider } from "react-redux";
 import { GlobalStyles } from "./styles/GlobalStyles";
-
-
-
+import { useState } from "react";
 
 function App(props: any) {
+  const [theme, setTheme] = useState("light");
+  console.log("theme", theme);
+
+  const toggletheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+    console.log("useEffect", theme);
+  }, [theme]);
+
   return (
     <Provider store={props.store}>
       <GlobalStyles />
       <div className="AppContainer">
         <PostsPage />
+        <button onClick={toggletheme}>смена темы</button>
       </div>
     </Provider>
   );
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -22,20 +22,43 @@ textarea {
     --font-size: 16px; 
     --primary-font: 'Nunito Sans', sans-serif;
     --base-line-height: 20px;
-    --background-color: #edeef0;
-    --background-post-color: #ffffff;
-    --background-like-color:#f0f2f5;
-    --background-like-text-color:#6b6d5d;
-    --text-color:#000001;
-    --text-color-hover:#5184bf;
+    // --background-color: #edeef0;
+    // --background-post-color: #ffffff;
+    // --background-like-color:#f0f2f5;
+    // --background-like-text-color:#6b6d5d;
+    // --text-color:#000001;
+    // --text-color-hover:#5184bf;
 }
+
+
+body[data-theme="light"] {
+  --background-color:#edeef0;
+  --background-post-color: #ffffff;
+  --background-like-color:#f0f2f5;
+  --background-like-text-color:#6b6d5d;
+  --text-color:#000001;
+  --text-color-hover:#5184bf;
+
+}
+
+
+body[data-theme="dark"] {
+  --background-color:red;
+  --background-post-color: green;
+  --background-like-color:blue;
+  --background-like-text-color:grey;
+  --text-color:white;
+  --text-color-hover:black;
+
+}
+
 
 body {
     font-family: var(--primary-font); 
     font-size: 1rem; 
     line-height: var(--base-line-height); 
-    background-color: var(--background-color);
-    color:--var(--text-color)
+    // background-color: var(--background-color);
+    // color:--var(--text-color)
     display:flex;
 }
 
